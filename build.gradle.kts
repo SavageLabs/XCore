11af2d2b1e8a3ff8abe9d0f731ad59eba43fda10
ProSavage <naman0311@gmail.com>
2020-07-14 12:34:50 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm") version "1.3.72"
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+}
+
+group = "net.savagelabs"
+version = "v1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
+    maven("https://nexus.savagelabs.net/repository/maven-releases/")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.11.1")
+    implementation("com.cryptomorin:XSeries:6.0.0")
+    implementation("me.rayzr522:jsonmessage:1.2.0")
+    implementation("com.github.MinusKube:SmartInvs:master-SNAPSHOT")
+    implementation("com.deanveloper:skullcreator:2.0.0")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+
+    val build by existing {
+        dependsOn(shadowJar)
+    }
+
+
+    val shadowJar = named<ShadowJar>("shadowJar") {
+        exclude("META-INF/*.DSA")
+        exclude("META-INF/*.RSA")
+        archiveBaseName.set("XCore")
+    }
+}
